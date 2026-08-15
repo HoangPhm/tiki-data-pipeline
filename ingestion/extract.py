cdab0c54291e6ca00b8e4e9564ce3c194a959ff4
HoangPhm <164197891+HoangPhm@users.noreply.github.com>
2026-08-15 03:40:43 +0000

    modified

--- ingestion/extract.py
+++ ingestion/extract.py
@@ -1,2 +1,15 @@
-import request
+import time
+import os
+import boto3
+import pandas as pd
+import requests
 import tenacity
+from datetime import datetime 
+from io import BytesIO
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEFAULT_CATEGORY_ID = 8322
+DEFAULT_NUMBER_PAGES = 10
+
